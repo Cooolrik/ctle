2c9cfc3a7e5b55d13bfb44b35aa61989f5bef83e
Ulrik Lindahl <ulrik@ulriklindahl.com>
2023-02-18 12:13:30 +0100

    added status_type and value_type typenames to status_return

--- ctle/status_return.h
+++ ctle/status_return.h
@@ -7,16 +7,20 @@ namespace ctle
 	{
 	// represents a pair of result/status and return type for 
 	// functions which return a type along with a status 
-	template<class _StatusType, class _RetType> class status_return;
+	template<class _StatusType, class _ValueType> class status_return;
 
 	// general implementation
-	template<class _StatusType, class _RetType> class status_return
+	template<class _StatusType, class _ValueType> class status_return
 		{
 		static_assert( std::is_trivially_copyable<_StatusType>() , "_StatusType needs to be trivially copyable, like a plain old value, like bool, int or enum, or a class with a default copy ctor" );
 
+		public:
+			using status_type = _StatusType;
+			using value_type = _ValueType;
+
 		private:
 			_StatusType sstatus = {};
-			_RetType svalue = {};
+			_ValueType svalue = {};
 
 			status_return() = delete;
 
@@ -25,10 +29,10 @@ namespace ctle
 			status_return( status_return &&other ) = default;
 
 			status_return( _StatusType _status ) : sstatus(_status) {}
-			status_return( _StatusType _status, const _RetType &_value ) : sstatus(_status) , svalue(_value) {}
-			status_return( _StatusType _status, _RetType &&_value ) noexcept : sstatus(_status) , svalue(std::move(_value)) {}
-			status_return( const _RetType &_value ) : svalue(_value) {}
-			status_return( _RetType &&_value ) noexcept : svalue(std::move(_value)) {}
+			status_return( _StatusType _status, const _ValueType &_value ) : sstatus(_status) , svalue(_value) {}
+			status_return( _StatusType _status, _ValueType &&_value ) noexcept : sstatus(_status) , svalue(std::move(_value)) {}
+			status_return( const _ValueType &_value ) : svalue(_value) {}
+			status_return( _ValueType &&_value ) noexcept : svalue(std::move(_value)) {}
 			
 			// returns the status as a bool, requires that status can be converted to a bool implicitly
 			operator bool() const { return this->sstatus; }
@@ -38,8 +42,8 @@ namespace ctle
 			_StatusType status() const { return this->sstatus; }
 			
 			// get a reference to the value returned
-			_RetType &value() { return this->svalue; }
-			const _RetType &value() const { return this->svalue; }
+			_ValueType &value() { return this->svalue; }
+			const _ValueType &value() const { return this->svalue; }
 		};
 
 	// partial implementation when we only have a _StatusType
