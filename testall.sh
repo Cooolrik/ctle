1bfcecd3e3a0c60ce8e49becee882a3dec834dba
Ulrik Lindahl <ulrik@ulriklindahl.com>
2023-11-25 12:24:49 +0100

    linux fix

--- testall.sh
+++ testall.sh
@@ -7,7 +7,7 @@ mkdir -p build_c++11
 cd build_c++11
 cmake .. -DCMAKE_CXX_STANDARD=11 -DCTLE_BUILD_TESTS=ON
 cmake --build .
-if [ "$os" = "GNU/Linux" ]; then 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	./unit_tests > ../results_c++11
 else
 	./Debug/unit_tests > ../results_c++11
@@ -19,7 +19,7 @@ mkdir -p build_c++14
 cd build_c++14
 cmake .. -DCMAKE_CXX_STANDARD=14 -DCTLE_BUILD_TESTS=ON
 cmake --build .
-if [ "$os" = "GNU/Linux" ]; then 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	./unit_tests > ../results_c++14
 else
 	./Debug/unit_tests > ../results_c++14
@@ -31,7 +31,7 @@ mkdir -p build_c++17
 cd build_c++17
 cmake .. -DCMAKE_CXX_STANDARD=17 -DCTLE_BUILD_TESTS=ON
 cmake --build .
-if [ "$os" = "GNU/Linux" ]; then 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	./unit_tests > ../results_c++17
 else
 	./Debug/unit_tests > ../results_c++17
@@ -43,7 +43,7 @@ mkdir -p build_c++20
 cd build_c++20
 cmake .. -DCMAKE_CXX_STANDARD=20 -DCTLE_BUILD_TESTS=ON
 cmake --build .
-if [ "$os" = "GNU/Linux" ]; then 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	./unit_tests > ../results_c++20
 else
 	./Debug/unit_tests > ../results_c++20
