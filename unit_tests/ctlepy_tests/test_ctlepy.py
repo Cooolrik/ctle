75c02c8e306be0cdd4f20def3e885a43723bcc13
Ulrik Lindahl <ulrik@ulriklindahl.com>
2024-12-01 14:43:36 +0100

    * Added test and publishing code for pypi package

---
+++ unit_tests/ctlepy_tests/test_ctlepy.py
@@ -0,0 +1,21 @@
+import unittest
+import sys
+import os
+
+# Add the src directory to sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'code_gen')))
+
+# Now you can import your_module
+import ctlepy
+
+class TestFormattedOutput(unittest.TestCase):
+	def setUp(self):
+		os.makedirs('./outputs/TestFormattedOutput', exist_ok=True)
+
+	def test_basic_functionality(self):
+		f = ctlepy.formatted_output()
+		f.generate_license_header()
+		f.write_lines_to_file('./outputs/TestFormattedOutput/testout.txt')
+
+if __name__ == '__main__':
+	unittest.main()
